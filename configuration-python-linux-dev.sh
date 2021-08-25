0ae1918a92ab049b26574ab364862afe1bd47d9b
william koller <wkoller25@gmail.com>
2021-08-24 22:11:57 -0300

    chore: initial commit

---
+++ configuration-python-linux-dev.sh
@@ -0,0 +1,14 @@
+# install curl
+apt install curl -y
+
+# install git
+apt install git -y
+
+# install python
+apt install python3.8 python3.8-dev python3.8-venv \
+  python3-venv idle-python3.8 python3-pip virtualenv gcc \
+  default-libmysqlclient-dev libssl-dev -y
+
+snap install pycharm-community --classic
+
+echo '\nThanks for using this script.\n'
